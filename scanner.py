011422c7c296e2011338173d79a15ec1ca3ae425
Boro2001 <mikolajborowicz7@gmail.com>
2022-04-12 09:53:19 +0200

    compiler.py

---
+++ scanner.py
@@ -0,0 +1,18 @@
+class Scanner:
+    filename = "example.md"
+
+    def __init__(self, filepath):
+        self.filename = filepath
+        return
+
+    def scan(self):
+        file = open(file=self.filename, mode='r')
+        text = file.read()
+        file.close()
+        return text
+
+    def save(self, outputfile, output):
+        file = open(file=outputfile, mode='w')
+        file.write(output)
+        file.close()
+        return
