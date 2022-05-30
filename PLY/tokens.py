e98dae7ef985e8173bf01ff7366f46abd8c67d14
Mikołaj Borowicz <62395530+Boro2001@users.noreply.github.com>
2022-05-30 23:00:42 +0200

    tokeny i ply

--- PLY/tokens.py
+++ PLY/tokens.py
@@ -1,45 +1,59 @@
 from matplotlib.ft2font import HORIZONTAL
-
+from ply import lex
+from ply import yacc
 
 tokens = (
-    NEWLINE,
-    INDENT,
-    TABLE,
-    TEXT, 
-    TABLESPLITTER,
-    STRINGCHAR,
-    TABCHAR,
-    HEADING1,
-    HEADING2,
-    HEADING3,
-    HEADING4,
-    HEADING5,
-    HEADING6,
-    HORIZONTALLINE,
-    BOLD,
-    ITALIC,
-    STRIKE,
-    QUOTE,
-    UNORDEREDLIST,
-    ORDEREDLIST,
-    CODE
+    'NEWLINE',
+    'INDENT',
+    'TABLE',
+    'TEXT',
+    'TABLESPLITTER',
+    'STRINGCHAR',
+    'TABCHAR',
+    'HEADING1',
+    'HEADING2',
+    'HEADING3',
+    'HEADING4',
+    'HEADING5',
+    'HEADING6',
+    'HORIZONTALLINE',
+    'BOLD',
+    'ITALIC',
+    'STRIKE',
+    'QUOTE',
+    'UNORDEREDLIST',
+    'ORDEREDLIST',
+    'CODE',
 )
 
-t_HEADING1 = r"\#",
-t_HEADING2 = r"\##",
-t_HEADING3 = r"\###",
-t_HEADING4 = r"\####",
-t_HEADING5 = r"\#####"
-t_HEADING6 = r"\######"
+t_HEADING1 = r"\#"
+t_HEADING2 = r"\#\#"
+t_HEADING3 = r"\#\#\#"
+t_HEADING4 = r"\#\#\#\#"
+t_HEADING5 = r"\#\#\#\#\#"
+t_HEADING6 = r"\#\#\#\#\#\#"
+t_HORIZONTALLINE = r"(---|\*\*\*|___)"
+t_BOLD = r"(\*\*|__)"
+t_ITALIC = r"(\*|_)"
+#t_BLOCKQUOTES = r">"
+#t_LIST = r"\+"
+#t_BLOCKNODE = r"```"
+t_TEXT = r"[a-zA-z]+"
+t_TABLESPLITTER = r"\|"
 
-t_horizontalline = r"(---|\*\*\*|___)"
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-t_bold = r"(\*\*|__)"
-t_italic = r"(\*|_)"
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
-t_blockquotes = r">"
-t_list = r"\+"
-t_blocknode = r"```"
+def t_STRINGCHAR(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
 
 
-
+lexer = lex.lex()
+parser = yacc.yacc()
