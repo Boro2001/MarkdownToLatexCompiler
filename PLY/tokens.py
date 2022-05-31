2b309cef2ea5f8b17aebdf4dc950748aa569ca88
Mikołaj Borowicz <62395530+Boro2001@users.noreply.github.com>
2022-05-31 12:34:31 +0200

    fe

--- PLY/tokens.py
+++ PLY/tokens.py
@@ -50,17 +50,17 @@ t_QUOTELINE = r'GT?(LINECHAR+|LITERAL)+'
 t_LISTLINE = r'WS*(BULLET|LISTNUMBER)WS+LINECHAR*'
 t_PARAGRAPHLINE = r'INITIALPARACHAR(LINECHAR+|LITERAL)*'
 t_TABLEDELIMINATORCELL = r'PIPE?:?-+:?'
-t_OPEN_FENCE = r'``` -> mode(FENCED)'#
+t_OPEN_FENCE = r'```->mode(FENCED)'#
 t_IGNORE_WS = r'WS->skip'#
 t_NEWLINE = r'\r?\n'
-t_CELLTEXT = r'PIPE IGNORE_WS ( CELLCHAR+|LITERAL)*'
+t_CELLTEXT = r'PIPE IGNORE_WS(CELLCHAR+|LITERAL)*'
 t_BACKTICK = r'`'
 t_GT = r'>'
 t_HASH = r'\#'
 t_PIPE = r'\|'
 t_CELLCHAR = r'(ESCAPEDCHAR|ESCAPEDPIPE|WS|ALPHANUMERIC|PUNCTUATION)'
 t_ESCAPEDCHAR = r'\\'
-t_ALPHANUMERIC = r'a-zA-Z0-9'
+t_ALPHANUMERIC = r'[a-zA-Z0-9]'
 t_PUNCTUATION = r'[!"#$%&\'()*+,\-./:;<=>?@[\]^_{}]'
 t_ESCAPEDPIPE = r'\|'
 t_WS = r' \t'
@@ -69,8 +69,8 @@ t_BULLET = r'[-+*]'
 t_INITIALPARACHAR = r'~[#>|\n\r]'
 t_LITERALCHAR = r'~[`]'
 t_LINECHAR = r'~[\n\r`]'
-t_CLOSE_FENCE = r'``` (FENCED_NEWLINE|EOF) -> mode(DEFAULT_MODE)'
-t_TEXTLINE = r'INITIALTEXTCHAR TEXTCHAR*FENCED_NEWLINE'
+t_CLOSE_FENCE = r'```(FENCED_NEWLINE|EOF)->mode(DEFAULT_MODE)'
+t_TEXTLINE = r'INITIALTEXTCHARTEXTCHAR*FENCED_NEWLINE'
 t_INITIALTEXTCHAR = r'~[\n\r]'
 t_LINENUMBER = r'DIGIT+'
 t_IMPORT = r'import'
@@ -94,3 +94,21 @@ def t_error(t):
     t.lexer.skip(1)
 
 lexer = lex.lex()
+
+data = '''
++ Create a list by starting a line with `+`, `-`, or `*`
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+      + Facilisis in pretium nisl aliquet
+      - Nulla vo
+'''
+# Give the lexer some input
+lexer.input(data)
+ 
+ # Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
