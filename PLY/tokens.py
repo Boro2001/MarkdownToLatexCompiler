a8695349d813d9db0030be154b523eb117da5322
Mikołaj Borowicz <62395530+Boro2001@users.noreply.github.com>
2022-05-31 12:20:49 +0200

    cos

--- PLY/tokens.py
+++ PLY/tokens.py
@@ -3,43 +3,87 @@ from ply import lex
 from ply import yacc
 
 tokens = (
+    'LITERAL',
+    'HEADINGLINE',
+    'QUOTELINE',
+    'LISTLINE',
+    'PARAGRAPHLINE',
+    'TABLEDELIMINATORCELL',
+    'OPEN_FENCE',
     'NEWLINE',
-    'INDENT',
-    'TABLE',
-    'TEXT',
-    'TABLESPLITTER',
-    'STRINGCHAR',
-    'TABCHAR',
-    'HEADING1',
-    'HEADING2',
-    'HEADING3',
-    'HEADING4',
-    'HEADING5',
-    'HEADING6',
-    'HORIZONTALLINE',
-    'BOLD',
-    'ITALIC',
-    'STRIKE',
-    'QUOTE',
-    'UNORDEREDLIST',
-    'ORDEREDLIST',
-    'CODE',
+    'CELLTEXT',
+    'BACKTICK',
+    'GT',
+    'HASH',
+    'PIPE',
+    'CELLCHAR',
+    'ESCAPEDCHAR',
+    'ALPHANUMERIC',
+    'PUNCTUATION',
+    'ESCAPEDPIPE',
+    'WS',
+    'LISTNUMBER',
+    'BULLET',
+    'INITIALPARACHAR',
+    'LITERALCHAR',
+    'LINECHAR',
+    'CLOSE_FENCE',
+    'TEXTLINE',
+    'INITIALTEXTCHAR',
+    'LINENUMBER',
+    'IMPORT',
+    'FROM',
+    'TO',
+    'STRING',
+    'WORD',
+    'FENCED_NEWLINE',
+    'FENCED_IGNORE_WS',
+    'DIGIT',
+    'WORDCHAR',
+    'TEXTCHAR',
+    'IGNORE_WS',
 )
 
-t_HEADING1 = r"\#"
-t_HEADING2 = r"\#\#"
-t_HEADING3 = r"\#\#\#"
-t_HEADING4 = r"\#\#\#\#"
-t_HEADING5 = r"\#\#\#\#\#"
-t_HEADING6 = r"\#\#\#\#\#\#"
-t_HORIZONTALLINE = r"(---|\*\*\*|___)"
-t_BOLD = r"(\*\*|__)"
-t_ITALIC = r"(\*|_)"
-#t_BLOCKQUOTES = r">"
-#t_LIST = r"\+"
-#t_BLOCKNODE = r"```"
-t_TEXT = r"[a-zA-z]+"
-t_TABLESPLITTER = r"\|"
+t_LITERAL = r'BACKTICK?LITERALCHAR+?BACKTICK'
+t_HEADINGLINE = r'HASH+?LINECHAR+'
+t_QUOTELINE = r'GT?(LINECHAR+|LITERAL)+'
+t_LISTLINE = r'WS*(BULLET|LISTNUMBER)WS+LINECHAR*'
+t_PARAGRAPHLINE = r'INITIALPARACHAR(LINECHAR+|LITERAL)*'
+t_TABLEDELIMINATORCELL = r'PIPE?:?-+:?'
+t_OPEN_FENCE = r'``` -> mode(FENCED)'#
+t_IGNORE_WS = r'WS->skip'#
+t_NEWLINE = r'\r?\n'
+t_CELLTEXT = r'PIPE IGNORE_WS ( CELLCHAR+|LITERAL)*'
+t_BACKTICK = r'`'
+t_GT = r'>'
+t_HASH = r'\#'
+t_PIPE = r'\|'
+t_CELLCHAR = r'(ESCAPEDCHAR|ESCAPEDPIPE|WS|ALPHANUMERIC|PUNCTUATION)'
+t_ESCAPEDCHAR = r'\\'
+t_ALPHANUMERIC = r'a-zA-Z0-9'
+t_PUNCTUATION = r'[!"#$%&\'()*+,\-./:;<=>?@[\]^_{}]'
+t_ESCAPEDPIPE = r'\|'
+t_WS = r' \t'
+t_LISTNUMBER = r'[1-9][.)]'
+t_BULLET = r'[-+*]'
+t_INITIALPARACHAR = r'~[#>|\n\r]'
+t_LITERALCHAR = r'~[`]'
+t_LINECHAR = r'~[\n\r`]'
+t_CLOSE_FENCE = r'``` (FENCED_NEWLINE|EOF) -> mode(DEFAULT_MODE)'
+t_TEXTLINE = r'INITIALTEXTCHAR TEXTCHAR*FENCED_NEWLINE'
+t_INITIALTEXTCHAR = r'~[\n\r]'
+t_LINENUMBER = r'DIGIT+'
+t_IMPORT = r'import'
+t_FROM = r'from'
+t_TO = r'to'
+t_STRING = r'\'LINECHAR*?\''
+t_WORD = r'WORDCHAR+'
+t_FENCED_NEWLINE = r'\r?\n'
+t_FENCED_IGNORE_WS = r'WS -> skip' #
+t_DIGIT = r'[0-9]'
+t_WORDCHAR = r'~[\n\r\t |`]'
+t_TEXTCHAR = r'~[\n\r]'
+
 
 def t_newline(t):
     r'\n+'
@@ -49,11 +93,4 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-def t_STRINGCHAR(t):
-    r'\d+'
-    t.value = int(t.value)    
-    return t
-
-
 lexer = lex.lex()
-parser = yacc.yacc()
