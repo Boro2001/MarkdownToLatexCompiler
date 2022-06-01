e7214a5063301c74ff9c82f890147ef55cf2f657
Bartosz Sambór <72545684+BartoszSambor@users.noreply.github.com>
2022-06-01 20:20:58 +0200

    word sentence and head working

--- PLYZdania/prototype.py
+++ PLYZdania/prototype.py
@@ -25,8 +25,21 @@ def t_error(t):
 def p_document(p):
     """
     document : sentence
+    | heading
+    | heading document
+    | sentence document
     """
-    p[0] = "sentence{" + p[1] + "}"
+    try:
+        if p[1][0:4] == "head":
+            p[0] = p[1] + p[2]
+        else:
+            p[0] = "sentence{" + p[1] + "}" + p[2]
+
+    except:
+        if p[1][0:4] == "head":
+            p[0] = p[1]
+        else:
+            p[0] = "sentence{" + p[1] + "}"
 
 
 def p_word(p):
@@ -60,7 +73,7 @@ def p_heading(p):
     """
     heading : HEADING1 sentence
     """
-    p[0] = p[1] + p[2]
+    p[0] = "head{" + p[2] + "}"
 
 
 # def p_error(p):
@@ -69,7 +82,8 @@ def p_heading(p):
 
 
 lexer = lex.lex()
-data = "ala je kota\n"
+data = "Ala je kota\n\n#Tytul lorem ipsum\nKot je Ale\n"
+#data = "ala je kota kot je ale\n#Tytul lorem ipsum\ndruga linia\n"
 # Give the lexer some input
 lexer.input(data)
 parser = yacc.yacc()
